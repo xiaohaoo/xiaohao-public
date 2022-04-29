976f1ecd61a405cf2f905f2dc7df3a59ef7fc320
小浩 <sdwenhappy@163.com>
2022-04-29 20:31:41 +0800

    feat: 🚀添加ResponseResult

--- src/main/java/com/xiaohaoo/common/web/ResponseResult.java
+++ src/main/java/com/xiaohaoo/common/web/ResponseResult.java
@@ -56,7 +56,6 @@ public class ResponseResult<T> {
         return new ResponseResult<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), ResponseMessage.INTERNAL_SERVER_ERROR.value());
     }
 
-
     public int getStatus() {
         return status;
     }
